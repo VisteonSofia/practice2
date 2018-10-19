b18a997823be2a066dbb4626559f62f03eb3735d
mharizanov <mharizanov@gmail.com>
2018-10-19 14:43:27 +0300

    PDU definitions initial

---
+++ FW/main/ecu_extract.h
@@ -0,0 +1,8 @@
+struct PDU1_Struct {
+  uint16_t AmbientTemp; 
+  uint8_t  DispSpeed;  // (0-160, step 1; 255 = SNA)
+  uint32_t DispDist;
+  uint16_t HMI_right_blinker_state;  // 0 = deactivated, 1 = active, 2 = RES, 3 = SNA
+  uint16_t HMI_left_blinker_state;   // 0 = deactivated, 1 = active, 2 = RES, 3 = SNA
+  uint8_t padding[21];  // Reduce as needed!!!!!!!!!!!!!!
+} PDU1_storage;
