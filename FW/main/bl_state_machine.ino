67be196f44e3e6472d8a6d421321506d6aa06400
Skrabul <ivaylo_mihov@abv.bg>
2018-12-06 18:49:56 +0200

    little rework of master and blinkers

--- FW/main/bl_state_machine.ino
+++ FW/main/bl_state_machine.ino
@@ -2,13 +2,48 @@
   
   #ifdef BLINKERS
   
-//  #define BL_DEBUG 1
+//  #define BL_DEBUG 1 //You will need SERIAL in dummies_master
+  #define buttonLeft  6  // Pin for left blink button
+  #define buttonRight 5 // Pin for right blink button
+  #define buttonHazzards 3 // Pin for Hazzards blink button
   
   #define BS_FEATURE_CYCLE_TIME 50
   #define BS_ACTIVE_TIME 600
   #define BS_WAITING_TIME 1000
+
+  enum blinker_state {BS_INIT, BS_IDLE, BS_LEFT, BS_RIGHT, BS_HAZZARDS, BS_WAIT, BS_FAIL} bs_stateVariable;
+  enum blinker_event {BE_NONE, BE_LEFT_BTN_PRESS, BE_RIGHT_BTN_PRESS, BE_HAZZARDS_BTN_PRESS , BE_FAIL} be_eventVariable;
+
+  uint32_t bs_msCounts;
+  uint32_t bs_prevMillis;
   
+  int bl_left_btn_press;
+  int bl_left_btn_prev_state;
+  int bl_right_btn_press;
+  int bl_right_btn_prev_state;
+  int bl_hazzards_btn_press;
+  int bl_hazzards_btn_prev_state;
   
+  int active_time;
+  int waiting_time;
+  void blinkers_init(){
+    pinMode(buttonLeft, INPUT_PULLUP);  // Initialize left button pin as INPUT with internal pullup enabled. This means we can skip the external pullup
+    pinMode(buttonRight, INPUT_PULLUP);
+    pinMode(buttonHazzards, INPUT_PULLUP);
+  
+    bl_left_btn_press = RELEASED;
+    bl_right_btn_press = RELEASED;
+    bl_left_btn_prev_state = RELEASED;
+    bl_right_btn_prev_state = RELEASED;
+    bl_hazzards_btn_press = RELEASED;
+    bl_hazzards_btn_prev_state = RELEASED;
+  
+    be_eventVariable = BE_NONE;
+    bs_stateVariable = BS_IDLE;
+    bs_msCounts = 0;
+    waiting_time = 0;
+
+  }
   void blinker_state_machine() {
   
     if (bs_prevMillis != millis()) {
--- FW/main/dummies_master.ino
+++ FW/main/dummies_master.ino
@@ -1,51 +1,24 @@
 #define IPC
 #define BLINKERS
+#define DIAG
+#define INT_SERIAL
 
-#define buttonLeft  6  // Pin for left blink button
-#define buttonRight 5 // Pin for right blink button
-#define buttonHazzards 3 // Pin for Hazzards blink button
+#include "diag.h"
 
-#define PRESSED 0
-#define RELEASED 1
-
-enum blinker_state {BS_INIT, BS_IDLE, BS_LEFT, BS_RIGHT, BS_HAZZARDS, BS_WAIT, BS_FAIL} bs_stateVariable;
-enum blinker_event {BE_NONE, BE_LEFT_BTN_PRESS, BE_RIGHT_BTN_PRESS, BE_HAZZARDS_BTN_PRESS , BE_FAIL} be_eventVariable;
-
-uint32_t bs_msCounts;
-uint32_t bs_prevMillis;
-
-int bl_left_btn_press;
-int bl_left_btn_prev_state;
-int bl_right_btn_press;
-int bl_right_btn_prev_state;
-int bl_hazzards_btn_press;
-int bl_hazzards_btn_prev_state;
-
-int active_time;
-int waiting_time;
+#define PRESSED 0 //buttons state
+#define RELEASED 1 //buttons state
 
 void setup() {
-
+#ifdef INT_SERIAL
   Serial.begin(115200);
   Serial.println("Starting..");
   Serial.flush();
+#endif
   
-  pinMode(buttonLeft, INPUT_PULLUP);  // Initialize left button pin as INPUT with internal pullup enabled. This means we can skip the external pullup
-  pinMode(buttonRight, INPUT_PULLUP);
-  pinMode(buttonHazzards, INPUT_PULLUP);
-
-  bl_left_btn_press = RELEASED;
-  bl_right_btn_press = RELEASED;
-  bl_left_btn_prev_state = RELEASED;
-  bl_right_btn_prev_state = RELEASED;
-  bl_hazzards_btn_press = RELEASED;
-  bl_hazzards_btn_prev_state = RELEASED;
-
-  be_eventVariable = BE_NONE;
-  bs_stateVariable = BS_IDLE;
-  bs_msCounts = 0;
-  waiting_time = 0;
-
+#ifdef DIAG
+  loadConfig();
+  delay(100);
+#endif
 
   
 }
@@ -54,6 +27,7 @@ void loop() {
 
   blinker_state_machine();
   ipc_state_machine();
+  
   delay(10);
 
 }
