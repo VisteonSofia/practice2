c83d6e1c8415cf75b28e113534a2257189f8d46c
VDech <39947137+VDech@users.noreply.github.com>
2018-12-17 18:09:09 +0200

    Update according to latestPDU

--- FW/hmi/IF1.ino
+++ FW/hmi/IF1.ino
@@ -1,20 +1,33 @@
 #ifdef HMI
-
-float getHMItemperature()
+int getTemperature()
 { 
-  return ((float)(PDU1_storage.AmbientTemp - 999)/10);
-  }
-bool getHMItempStat()
-{
-  return (PDU1_storage.AmbientTemp == AT_SNA);
-  }
-uint32_t getHMItripmeter()
+  return (PDU1_storage.DispTemp);
+uint8_t getTempUnit()
 { 
-  return (PDU1_storage.DispDist/100);
+  return (PDU1_storage.UnitTemp);
   }
-uint8_t getHMIspeed()
+uint32_t getOdometer()
+{ 
+  return (PDU1_storage.DispDist);
+  }
+uint16_t getMaxSpeed()
+{ 
+  return (PDU1_storage.MaxSpeed);
+  }
+uint16_t getSpeed()
 { 
   return (PDU1_storage.DispSpeed);
   }
-
- #endif
+uint8_t getSpeedUnit()
+{ 
+  return (PDU1_storage.UnitSpeed);
+  }
+uint8_t getBlinkerLeft()
+{ 
+  return (PDU1_storage.HMI_left_blinker_state);
+  }
+uint8_t getBlinkerRight()
+{ 
+  return (PDU1_storage.HMI_right_blinker_state);
+  }
+#endif
