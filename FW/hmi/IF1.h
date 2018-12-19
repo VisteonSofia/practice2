2d73a42e50f5d916f812cb6812a527049ceb035f
Skrabul <ivaylo_mihov@abv.bg>
2018-12-19 11:11:25 +0200

    Delete IF1.h

--- FW/hmi/IF1.h
+++
@@ -1,8 +0,0 @@
-int getTemperature();
-uint8_t getTempUnit();
-uint32_t getOdometer();
-uint16_t getSpeed();
-uint16_t getMaxSpeed();
-uint8_t getSpeedUnit();
-uint8_t getBlinkerLeft();
-uint8_t getBlinkerRight();
